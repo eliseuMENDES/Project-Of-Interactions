151aaea966e00ba4d8e6d38f39f5f958cd383eac
eliseuMendes <eliseu839@gmail.com>
2025-03-06 12:39:35 -0300

    Implementação em melhorias no estilo e interação

--- myproject/js/script.js
+++ myproject/js/script.js
@@ -1,4 +1,13 @@
-const themeToggle = document.getElementById("theme-toggle");
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-})
+// const themeToggle = document.getElementById("theme-toggle");
+// themeToggle.addEventListener("click", () => {
+//     document.body.classList.toggle("dark");
+// })
+
+document.querySelectorAll('a[href^="#"').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollInToView({
+            behavior: 'smooth'
+        });
+    });
+});
