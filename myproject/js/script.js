268b47e2a05d71695542d38548a3b5d8c981d1e0
eliseuMendes <eliseu839@gmail.com>
2025-03-06 14:32:24 -0300

    Alterando Projeto

--- myproject/js/script.js
+++ myproject/js/script.js
@@ -3,11 +3,24 @@
 //     document.body.classList.toggle("dark");
 // })
 
-document.querySelectorAll('a[href^="#"').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollInToView({
-            behavior: 'smooth'
-        });
-    });
+// document.querySelectorAll('a[href^="#"').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollInToView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+const input = document.getElementById('user-input');
+const button = document.getElementById('send-button');
+const resultDiv = document.getElementById('result');
+
+button.addEventListener('click', () => {
+    const inputValue = input.value;
+    if (inputValue) {
+        resultDiv.textContent = `Você digitou: ${inputValue}`
+    } else {
+        resultDiv.textContent = 'Por favor, digite algo.';
+    }
 });
